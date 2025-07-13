87e184cf67bf136022a14fb393ceab00ed59e56d
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2025-07-12 22:08:46 -0400

    Setup components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { Home } from './Home.jsx'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Tron 2025 Class Profile</h1>
-        <p>Coming Soon</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
     </div>
   );
 }
